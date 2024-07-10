7dff96905f2ef53cc2c7d16d97dfbb2f4a18d877
1f10220175 <s1f102201751@iniad.org>
2024-07-10 22:22:15 +0900

    トップページを追加

--- todo/tests.py
+++ todo/tests.py
@@ -31,6 +31,7 @@ class TaskModelTestCase(TestCase):
     def test_is_overdue_feture(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
+        task = Task(title='task1', due_at=due)
         task.save()
 
         self.assertFalse(task.is_overdue(current))
